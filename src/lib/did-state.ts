0203b518dbc519e42cc0b9a54cab06a170df0bf9
Julio Cabrapan Duarte <juliocabdu@pm.me>
2020-07-19 22:01:20 +0200

    did state

---
+++ src/lib/did-state.ts
@@ -0,0 +1,21 @@
+/*
+    TyronZIL-js: Decentralized identity client for the Zilliqa blockchain platform
+    Copyright (C) 2020 Julio Cesar Cabrapan Duarte
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
+export default class DidState {
+    /** Fetches the current state for the given DID */
+    public static async fetch (did: tyronZILScheme) {}
+    /** Applies the new state to the given DID */
+    public static async applyCreate() {}
+}
